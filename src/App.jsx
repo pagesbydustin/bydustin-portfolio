256d02fdbf0e50b52d5e9228111782225a883a71
Dustin <pagesbydustin>
2024-08-14 16:00:12 -0700

    working data and start of storage of resume json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import HeaderComponent from "./components/layout/HeaderComponent"
-import NavigationComponent from "./components/layout/NavigationComponent"
-import BodyLayoutComponent from "./components/layout/BodyLayoutComponent"
+import HeaderComponent from "./components/layout/HeaderComponent";
+import NavigationComponent from "./components/layout/NavigationComponent";
+import BodyLayoutComponent from "./components/layout/BodyLayoutComponent";
+import Resume from "./assets/Resume-Data_2024.json";
 
 function App() {
-
+  const resumeData = Resume;
+  const Person = {
+    name: resumeData.Name,
+    address: resumeData.Address,
+    phone: resumeData.Phone,
+    summary: resumeData["Professional Summary"],
+    social: { linkedIn: resumeData.LinkedIn },
+  };
+  Person.store = (key, value) => {
+    sessionStorage.setItem(key, value);
+  };
+  Person.store("Person", JSON.stringify(Person));
   return (
     <>
-      <NavigationComponent />{/** Nav Component */}
-      <div id="mainContent" className='container clearfix bg-primary-subtle p1'>
-        <HeaderComponent propsTitle={"Welcome"} propsHeight={110} />{/** Header Component */}
-        <BodyLayoutComponent welcomeMsg={"Portfolio"} />{/** Body Layout Component */}
+      <NavigationComponent />
+      {/** Nav Component */}
+      <div id="mainContent" className="container clearfix bg-primary-subtle p1">
+        <HeaderComponent propsTitle={Person.name} propsHeight={110} />
+        {/** Header Component */}
+        <BodyLayoutComponent />
+        {/** Body Layout Component */}
         {/** Footer Component */}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
