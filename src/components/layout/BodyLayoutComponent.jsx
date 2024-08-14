acb9be00889919c8cd2a5b1865bb03d5cf218fde
Dustin <pagesbydustin>
2024-08-14 16:00:12 -0700

    working data and start of storage of resume json data

--- src/components/layout/BodyLayoutComponent.jsx
+++ src/components/layout/BodyLayoutComponent.jsx
@@ -1,23 +1,42 @@
-import PropTypes from 'prop-types';
-import { Col, Row } from "react-bootstrap";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { Card, Col, ListGroup, ListGroupItem, Row } from "react-bootstrap";
 
 export default function BodyLayoutComponent() {
-    return (
-        <>
-            <div className={'container-flex-full my-2'}>
-                <Row>
-                    <Col className=" p-2 col-100" lg={100}>
-                        <div className="align-center" id="home">
-                            <h3 id='home' style={{ "textAlign": "Center" }}>
-                                Dustin Parker
-                            </h3>
-                        </div>
-                    </Col>
-                    <Col></Col>
-                </Row>
+  const [mouseIsOver, setMouseIsOver] = useState(null);
+
+  function setBgColor(item) {
+    setMouseIsOver(item);
+  }
+
+  return (
+    <>
+      <div className={"container-flex-full my-2"}>
+        <Row>
+          <Col className=" p-2 col-100" lg={100}>
+            <div className="align-center" id="home">
+              <h3 id="home" style={{ textAlign: "Center" }}>
+                Who is this guy?
+              </h3>
             </div>
-        </>
-
-    );
+          </Col>
+          <Col className="p-2 col-100">
+            <Card className="card-subtitle">
+              <Card.Header className="text-center">
+                <b>- About Dustin -</b>
+              </Card.Header>
+              <Card.Body>
+                <Row>
+                  <Col lg={1} md={12}>
+                    test
+                  </Col>
+                  <Col></Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
 }
-
--- src/components/layout/NavigationComponent.jsx
+++ src/components/layout/NavigationComponent.jsx
@@ -1,39 +1,54 @@
-
-import { Nav, Navbar, Container, Offcanvas } from 'react-bootstrap'
+import { Nav, Navbar, Container, Offcanvas } from "react-bootstrap";
 
 export default function NavigationComponent() {
-    return (
-
-
-        <Navbar fixed='top' key={'sm'} expand={"sm"} variant='dark' className="bg-dark mb-3">
-            <Container fluid>
-                <Navbar.Brand href="#">
-                    <img src='/logo.png' height={"50"} className='rounded-circle' />
-                    <span>{" - "}</span>By Dustin - </Navbar.Brand>
-                <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} />
-                <Navbar.Offcanvas
-                    id={`offcanvasNavbar-expand-sm`}
-                    aria-labelledby={`offcanvasNavbarLabel-expand-sm`}
-                    placement="end"
-
-                >
-                    <Offcanvas.Header className='' closeButton>
-                        <Offcanvas.Title className='' id={`offcanvasNavbarLabel-expand-sm`}>
-                            Main Menu
-                        </Offcanvas.Title>
-                    </Offcanvas.Header>
-                    <Offcanvas.Body>
-                        <Nav className="justify-content-end flex-grow-1 pe-3" defaultActiveKey={"home"}>
-                            <Nav.Link href="#action1" active key={"home"}>Home</Nav.Link>
-                            <Nav.Link href="#action2" key={"about"}>About</Nav.Link>
-                            <Nav.Link href="#action3" key={"skills"}>Skills</Nav.Link>
-                            <Nav.Link href="#action4" key={"experiance"}>Experiance</Nav.Link>
-                            <Nav.Link href="#action5" key={"contact"}>Contact</Nav.Link>
-                        </Nav>
-                    </Offcanvas.Body>
-                </Navbar.Offcanvas>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar
+      fixed="top"
+      key={"sm"}
+      expand={"sm"}
+      variant="dark"
+      className="bg-dark mb-3"
+    >
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <img src="/logo.png" height={"50"} className="rounded-circle" />
+          <span>{" - "}</span>By Dustin -{" "}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-sm`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-sm`}
+          placement="end"
+        >
+          <Offcanvas.Header className="" closeButton>
+            <Offcanvas.Title className="" id={`offcanvasNavbarLabel-expand-sm`}>
+              Main Menu
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav
+              className="justify-content-end flex-grow-1 pe-3"
+              defaultActiveKey={"home"}
+            >
+              <Nav.Link href="#home" key={"home"}>
+                Home
+              </Nav.Link>
+              <Nav.Link href="#about" key={"about"}>
+                About
+              </Nav.Link>
+              <Nav.Link href="#skills" key={"skills"}>
+                Skills
+              </Nav.Link>
+              <Nav.Link href="#experiance" key={"experiance"}>
+                Experiance
+              </Nav.Link>
+              <Nav.Link href="#contact" key={"contact"}>
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
 }
-
